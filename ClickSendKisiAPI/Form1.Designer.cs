659ce576c7eb8f107334a12ce9a90b82bc84f349
michael.sharich@gmail.com <michael.sharich@gmail.com>
2023-11-21 11:03:43 -0700

    Add project files.

---
+++ ClickSendKisiAPI/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ClickSendKisiAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_GetEvents = new System.Windows.Forms.Button();
+            this.LB_Events = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btn_GetEvents
+            // 
+            this.btn_GetEvents.Location = new System.Drawing.Point(413, 84);
+            this.btn_GetEvents.Name = "btn_GetEvents";
+            this.btn_GetEvents.Size = new System.Drawing.Size(163, 66);
+            this.btn_GetEvents.TabIndex = 0;
+            this.btn_GetEvents.Text = "Get Events";
+            this.btn_GetEvents.UseVisualStyleBackColor = true;
+            this.btn_GetEvents.Click += new System.EventHandler(this.btn_GetEvents_Click);
+            // 
+            // LB_Events
+            // 
+            this.LB_Events.FormattingEnabled = true;
+            this.LB_Events.ItemHeight = 20;
+            this.LB_Events.Location = new System.Drawing.Point(108, 181);
+            this.LB_Events.Name = "LB_Events";
+            this.LB_Events.Size = new System.Drawing.Size(794, 324);
+            this.LB_Events.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1152, 580);
+            this.Controls.Add(this.LB_Events);
+            this.Controls.Add(this.btn_GetEvents);
+            this.Name = "Form1";
+            this.Text = "ClickSendKisiAPI";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_GetEvents;
+        private System.Windows.Forms.ListBox LB_Events;
+    }
+}
+
---
+++ ClickSendKisiAPI/Form1.cs
@@ -0,0 +1,85 @@
+﻿using IO.ClickSend.ClickSend.Api;
+using IO.ClickSend.ClickSend.Model;
+using IO.ClickSend.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClickSendKisiAPI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeHttpClient();
+            InitializeClickSend();
+        }
+
+        SMSApi SMSAPI;
+        HttpClient HClient;
+
+        public void InitializeHttpClient()
+        {
+            HClient = new HttpClient()
+            {
+                BaseAddress = new Uri("https://api.kisi.io//")
+            };
+
+            HClient.DefaultRequestHeaders.Add("Authorization", "Basic " + "Kisi_API_Key");
+            HClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
+        }
+
+        public void InitializeClickSend()
+        {
+            Configuration config = new Configuration();
+            config.Username = "clicksend@clicksend.com";
+            config.AddApiKey("0", "2F19C229-6167-9AFD-22C0-8222222222");
+
+            SMSAPI = new SMSApi(config);
+
+
+        }
+
+        public async void GetAsyncEvents(HttpClient InClient)
+        {
+            //Get events response from Kisi
+            HttpResponseMessage response = await InClient.GetAsync("events");
+
+            response.EnsureSuccessStatusCode();
+
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            LB_Events.Items.Add(jsonResponse);
+        }
+
+        private void btn_GetEvents_Click(object sender, EventArgs e)
+        {
+            GetAsyncEvents(HClient);
+        }
+
+        private void SendClickSendSMS()
+        {
+            //Example of sending SMS from ClickSend
+            var listOfSMS = new List<SmsMessage>
+            {
+                new SmsMessage(
+                    to: "Test",
+                    body: "Test Message",
+                    from: "",
+                    source: "sdk",
+                    schedule: 0
+                    )
+            };
+
+            var SMSCollesction = new SmsMessageCollection(listOfSMS);
+            string response = SMSAPI.SmsSendPost(SMSCollesction);
+        }
+    }
+}
